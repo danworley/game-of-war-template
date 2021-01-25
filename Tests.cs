a9627415354d31728913c210d505f77bcad30e9e
danworley <daniel-worley@pluralsight.com>
2021-01-25 12:28:31 -0700

    Initial check-in.

---
+++ Tests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace GameOfWar
+{
+    public class Tests
+    {
+        [Test]
+        public void Can_test()
+        {
+            Assert.Pass();
+        }
+    }
+}
